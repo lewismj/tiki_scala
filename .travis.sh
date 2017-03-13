abe8edd5973d800eaf472cfe2204da8bff7c5ea9
Michael Lewis <lewismj@waioeka.com>
2017-03-13 23:49:55 +0800

    update .travis

--- .travis.sh
+++ .travis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sbt_cmd="sbt ++$TRAVIS_SCALA_VERSION"
-coverage="(export SCOVERAGEON=true; $sbt_cmd coverage testsJVM/test coverageReport && bash <(curl -s https://codecov.io/bash) )"
+coverage="(export SCOVERAGEON=true; $sbt_cmd coverage tests/test coverageReport && bash <(curl -s https://codecov.io/bash) )"
 all="$sbt_cmd clean compile" # && $sbt_cmd tests/test"
 
 run_cmd="$coverage && all"
